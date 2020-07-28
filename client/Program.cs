a2e50f3d58780efe063e36e5575b6782b9cb9325
Mike Rousos <Mike@MJRousos.com>
2020-07-28 17:50:15 -0400

    Update client to call server API

--- client/Program.cs
+++ client/Program.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace client
 {
     class Program
     {
-        static void Main(string[] args)
+        private static IConfiguration Configuration { get; } = GetConfiguration();
+
+        private static async Task Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Calling protected web API...");
+            await CallWebApiAsync();
+
+            Console.WriteLine("Done");
+        }
+
+        private static async Task CallWebApiAsync()
+        {
+            var client = new HttpClient();
+            var message = new HttpRequestMessage(HttpMethod.Get, "https://localhost:5001/WeatherForecast");
+            message.Headers.Authorization = new AuthenticationHeaderValue("Bearer", Configuration["jwt"]);
+            var response = await client.SendAsync(message);
+            Console.WriteLine($"Response: {response.StatusCode}");
+        }
+
+        private static IConfiguration GetConfiguration()
+        {
+            return new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true)
+                .AddEnvironmentVariables()
+                .AddUserSecrets<Program>()
+                .Build();
         }
     }
 }
